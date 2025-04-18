07bd22947ec9f53091dd4fae604a489db3b5f96d
Jay <jaisarkari9@gmail.com>
2025-04-18 17:48:35 +0530

    commit

--- src/components/ProjectShowSection.tsx
+++ src/components/ProjectShowSection.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import ProjectShowcaseBlock from './ui/ProjectShowcaseBlock'
+'use client';
+
+import React, { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import ProjectShowcaseBlock from './ui/ProjectShowcaseBlock';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ProjectShowSection = () => {
+  const triggerRef = useRef(null);
+  const [isInView, setIsInView] = useState(false);
+
+  useEffect(() => {
+    const trigger = triggerRef.current;
+
+    ScrollTrigger.create({
+      trigger: trigger,
+      start: 'top 80%',
+      onEnter: () => setIsInView(true),
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach(t => t.kill());
+    };
+  }, []);
+
   return (
-    <div className='mb-16 flex items-center justify-center'>
-      <ProjectShowcaseBlock/>
+    <div ref={triggerRef} className="mb-16 flex items-center justify-center min-h-[300px]">
+      {isInView && <ProjectShowcaseBlock />}
     </div>
-  )
-}
-export default ProjectShowSection
+  );
+};
+
+export default ProjectShowSection;
